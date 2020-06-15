d4fe30b00bdeca21f22232e36f01d97c5d88fb99
Sayan Mitra <sayanmitra@Sayans-MacBook-Air.local>
2020-06-15 23:03:22 +0530

    fixed compilation bug of auto encoder, but issues in decoding

--- data_autoencoder.py
+++ data_autoencoder.py
@@ -12,7 +12,7 @@ train = np.loadtxt(filename, delimiter = ',', dtype = 'float32')
 train = train.reshape(-1, 13, 2)
 #print(train[:,0,:])
 train_tensor = torch.from_numpy(train)
-print(train_tensor)
+#print(train_tensor)
 
 num_epochs = 100
 batch_size = 128
@@ -22,9 +22,11 @@ class autoencoder(nn.Module):
     def __init__(self):
         super(autoencoder, self).__init__()
         self.encoder = nn.Sequential(
-            nn.Linear(28 * 28, 128),
+            #nn.Linear(28 * 28, 128),
+            nn.Linear(2, 325),
             nn.ReLU(True),
-            nn.Linear(128, 64),
+            #nn.Linear(128, 64),
+            nn.Linear(325, 64),
             nn.ReLU(True), nn.Linear(64, 12), nn.ReLU(True), nn.Linear(12, 3))
         self.decoder = nn.Sequential(
             nn.Linear(3, 12),
@@ -32,7 +34,7 @@ class autoencoder(nn.Module):
             nn.Linear(12, 64),
             nn.ReLU(True),
             nn.Linear(64, 128),
-            nn.ReLU(True), nn.Linear(128, 28 * 28), nn.Tanh())
+            nn.ReLU(True), nn.Linear(128, 2), nn.Tanh())#nn.Linear(128, 28 * 28), nn.Tanh())
 
     def forward(self, x):
         x = self.encoder(x)
@@ -48,9 +50,9 @@ optimizer = torch.optim.Adam(
 for epoch in range(num_epochs):
     #for data in dataloader:
     img = train_tensor
-    img = img.view(img.size(0), -1)
+    #img = img.view(img.size(0), -1)
     img = Variable(img) #.cuda()
-    print(img)
+    #print(img)
         # ===================forward=====================
     output = model(img)
     loss = criterion(output, img)
@@ -59,5 +61,4 @@ for epoch in range(num_epochs):
     loss.backward()
     optimizer.step()
     # ===================log========================
-    #print('epoch [{}/{}], loss:{:.4f}'
-          #.format(epoch + 1, num_epochs, loss.data[0]))
+    print(output)
