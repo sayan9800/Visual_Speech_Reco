22272f0b284b3bc3bc47733c579c3f4a122e45e5
Sayan Mitra <sayanmitra@Sayans-MacBook-Air.local>
2020-06-19 18:56:42 +0530

    Fixed Autoencoder

--- data_autoencoder.py
+++ data_autoencoder.py
@@ -1,64 +1,71 @@
-import os
 import numpy as np
 import torch
-import torchvision
 from torch import nn
-from torch.autograd import Variable
-from torchvision import transforms
+from matplotlib import pyplot as plt
 
 filename = 'Thank_You.txt'
 
 train = np.loadtxt(filename, delimiter = ',', dtype = 'float32')
-train = train.reshape(-1, 13, 2)
-#print(train[:,0,:])
-train_tensor = torch.from_numpy(train)
-#print(train_tensor)
+#Testing the x-axis data
+train_tensor = torch.from_numpy(train[:,0])
 
-num_epochs = 100
+#Hyperparameters
+num_epochs = 150
 batch_size = 128
 learning_rate = 1e-3
 
-class autoencoder(nn.Module):
-    def __init__(self):
-        super(autoencoder, self).__init__()
+class Autoencoder(nn.Module):
+    def __init__(self, size, hidden1 = 128, hidden2 = 64, hidden3 = 32, hidden4 = 16):
+        super().__init__()
         self.encoder = nn.Sequential(
-            #nn.Linear(28 * 28, 128),
-            nn.Linear(2, 325),
-            nn.ReLU(True),
-            #nn.Linear(128, 64),
-            nn.Linear(325, 64),
-            nn.ReLU(True), nn.Linear(64, 12), nn.ReLU(True), nn.Linear(12, 3))
+                        nn.Linear(size, hidden1),
+                        nn.ReLU(True),
+                        nn.Linear(hidden1, hidden2),
+                        nn.ReLU(True),
+                        nn.Linear(hidden2, hidden3),
+                        nn.ReLU(True),
+                        nn.Linear(hidden3, hidden4))
+
         self.decoder = nn.Sequential(
-            nn.Linear(3, 12),
-            nn.ReLU(True),
-            nn.Linear(12, 64),
-            nn.ReLU(True),
-            nn.Linear(64, 128),
-            nn.ReLU(True), nn.Linear(128, 2))#, nn.Tanh())#nn.Linear(128, 28 * 28), nn.Tanh())
+                        nn.Linear(hidden4, hidden3),
+                        nn.ReLU(True),
+                        nn.Linear(hidden3, hidden2),
+                        nn.ReLU(True),
+                        nn.Linear(hidden2, hidden1),
+                        nn.ReLU(True),
+                        nn.Linear(hidden1, size))
 
     def forward(self, x):
         x = self.encoder(x)
         x = self.decoder(x)
         return x
 
-
-model = autoencoder() #.cuda()
-criterion = nn.MSELoss()
-optimizer = torch.optim.Adam(
-    model.parameters(), lr=learning_rate, weight_decay=1e-5)
+loss_data = []
+model = Autoencoder(size = len(train_tensor))
+loss_criterion = nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-5)
 
 for epoch in range(num_epochs):
-    #for data in dataloader:
-    img = train_tensor
-    #img = img.view(img.size(0), -1)
-    img = Variable(img) #.cuda()
-    #print(img)
-        # ===================forward=====================
-    output = model(img)
-    loss = criterion(output, img)
-        # ===================backward====================
+    #foreward
+    output = model(train_tensor)
+    loss = loss_criterion(output, train_tensor)
+
+    #backward
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
-    # ===================log========================
-    print(output)
+
+    #log
+    print('Epoch [{}/{}], Loss: {:.4f}'.format(epoch+1, num_epochs, loss.item()))
+    loss_data.append(loss.item())
+
+print(train_tensor)
+print(output)
+print('\n')
+encoded_tensor = model.encoder(train_tensor)
+print(encoded_tensor, len(encoded_tensor))
+plt.plot(range(num_epochs), loss_data)
+plt.ylabel('MSE LOSS')
+plt.xlabel('epochs')
+plt.grid(True)
+plt.show()
