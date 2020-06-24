ba1b3edcee1e590ddd711e13b9a7591c45427689
Sayan Mitra <ee18b156@smail.iitm.ac.in>
2020-06-24 23:09:02 +0530

    Added GPU processing to AE

--- data_autoencoder.py
+++ data_autoencoder.py
@@ -23,9 +23,38 @@ test_dataset = DataLoader(test_dataset, batch_size = 1, shuffle = True)
 #Testing the x-axis data
 
 #Hyperparameters
-num_epochs = 100
-batch_size = 128
-learning_rate = 1e-3
+num_epochs = 50
+#batch_size = 128
+learning_rate = 1e-4
+
+def get_default_device():
+    if torch.cuda.is_available():
+        return torch.device('cuda')
+    else:
+        return torch.device('cpu')
+
+def to_device(data, device):
+    if isinstance(data, (list, tuple)):
+        return [to_device(x, device) for x in data]
+    return data.to(device, non_blocking=True)
+
+device = get_default_device()
+
+class DeviceDataLoader():
+#Wrapper class to easily put data into GPU
+    def __init__(self, dl, device):
+        self.dl = dl
+        self.device = device
+
+    def __iter__(self): #while iterating put things one by one into the GPU
+        for b in self.dl:
+            yield to_device(b, self.device)
+
+    def __len__(self):
+        return len(self.dl)
+
+train_dataset = DeviceDataLoader(train_dataset, device)
+test_dataset = DeviceDataLoader(test_dataset, device)
 
 class Autoencoder(nn.Module):
     def __init__(self, size, hidden1 = 64, hidden2 = 32, hidden3 = 16, hidden4 = 8):
@@ -55,8 +84,9 @@ class Autoencoder(nn.Module):
 
 loss_data = []
 model = Autoencoder(size = 100)
-loss_criterion = nn.MSELoss()
+to_device(model, device)
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-5)
+loss_criterion = nn.MSELoss()
 
 for epoch in range(num_epochs):
 	for train in train_dataset:
